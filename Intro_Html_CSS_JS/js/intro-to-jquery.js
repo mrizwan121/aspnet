55386878d0876ffdb8adbba10b6def9f47b97170
powerdevpk <rizwan2009@live.com>
2017-02-16 23:27:56 -0800

    testing

---
+++ Intro_Html_CSS_JS/js/intro-to-jquery.js
@@ -0,0 +1,25 @@
+﻿/// <reference path="jquery-3.1.1.js" />
+
+$(function () {
+    
+
+    var count = $("#page").find("div").length;
+    var p_count = $("#page").find("p").length;
+
+
+    var images = $("img");
+    images.each(function () {
+        var currentImage = $(this);
+        currentImage.on("click", function () {            
+            currentImage.attr("width", "100%");
+
+        });
+    });
+
+    //var images =document.getElementsByTagName("img");
+
+
+
+
+
+});
---
+++ Intro_Html_CSS_JS/js/scripts.js
@@ -0,0 +1,11 @@
+﻿
+var btn = document.getElementById("btn1");
+
+
+
+btn.addEventListener("click", function () {
+    var div = document.createElement("div");
+    div.innerHTML = "<h1> Im being added by javascript! </h1>";
+
+
+});
